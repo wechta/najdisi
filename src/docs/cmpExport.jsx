2a212939515250ef02ecdd3af7b27210b5e32d16
wehtap <peter.wechtersbach@setcce.si>
2018-05-10 11:24:38 +0200

    Added apiUrl to cmpExport for all components.

--- src/docs/cmpExport.jsx
+++ src/docs/cmpExport.jsx
@@ -7,19 +7,19 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 const showNormalArticle = (apiUrl, element) => {
-    ReactDOM.render(<StandaloneArticle />, element);
+    ReactDOM.render(<StandaloneArticle apiUrl={apiUrl} />, element);
 };
 
 const showArticleSwitcher = (apiUrl, element) => {
-    ReactDOM.render(<ArticleSwitcher />, element);
+    ReactDOM.render(<ArticleSwitcher apiUrl={apiUrl} />, element);
 };
 
 const showArticleWithSub = (apiUrl, element) => {
-    ReactDOM.render(<ArticleWithSubarticles />, element);
+    ReactDOM.render(<ArticleWithSubarticles apiUrl={apiUrl} />, element);
 };
 
 const showBoxArticles = (apiUrl, element) => {
-    ReactDOM.render(<BoxArticle />, element);
+    ReactDOM.render(<BoxArticle apiUrl={apiUrl} />, element);
 };
 
 module.exports = {
